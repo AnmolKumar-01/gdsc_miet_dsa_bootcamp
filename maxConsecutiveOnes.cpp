81e0eb0ab9218ecfcdc20adce64922d4f331b609
Anmol Kumar <116261406+AnmolKumar-01@users.noreply.github.com>
2023-02-12 16:30:23 +0530

    Update maxConsecutiveOnes.cpp

--- maxConsecutiveOnes.cpp
+++ maxConsecutiveOnes.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-
+int maxConsecutiveOnes( int &N )
+{
+    int count = 0;
     
+    while( N != 0 ) {
+        N = N << 1;
+        count++ ;
+    }
+    
+    return count ;
+}
+
+int main(){
+    
+    int n = 10111001;
+    
+    cout<<maxConsecutiveOnes(n)<<endl;
     
     return 0;
 }
